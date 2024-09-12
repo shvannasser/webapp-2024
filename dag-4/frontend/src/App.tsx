93443a6dd4a19f8db0c159051363cb4cec833d16
GrapeBird <shvann@hiof.no>
2024-09-12 12:03:10 +0200

    day-4

---
+++ dag-4/frontend/src/App.tsx
@@ -0,0 +1,4 @@
+function App() {
+  return <h1>I know react</h1>
+}
+export default App
---
+++ t4_oppgave4_live/src/index.ts
@@ -0,0 +1,53 @@
+import { serve } from "@hono/node-server"
+import { Hono } from "hono"
+import { cors } from "hono/cors"
+
+const app = new Hono()
+
+app.use("/*", cors())
+
+const dadJokes = [
+  {
+    id: 1,
+    title: "Why don't scientists trust atoms?",
+    answer: "Because they make up everything!",
+  },
+  {
+    id: 2,
+    title: "What do you call a fake noodle?",
+    answer: "An impasta!",
+  },
+  {
+    id: 3,
+    title: "Why did the scarecrow win an award?",
+    answer: "He was outstanding in his field!",
+  },
+  {
+    id: 4,
+    title: "How do you organize a space party?",
+    answer: "You planet!",
+  },
+  {
+    id: 5,
+    title: "What do you call a bear with no teeth?",
+    answer: "A gummy bear!",
+  },
+]
+
+app.get("/", (c) => {
+  return c.json(dadJokes)
+})
+
+app.post("/post", async (c) => {
+  const body = await c.req.json()
+  dadJokes.push(body)
+  return c.json(dadJokes)
+})
+
+const port = 3999
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port,
+})
