c5e99af227671bb9325609143c93afb520d942fe
GrapeBird <shvann@hiof.no>
2024-09-26 11:22:48 +0200

    comments

--- live/dag-6/frontend/src/components/AddStudentForm.tsx
+++ live/dag-6/frontend/src/components/AddStudentForm.tsx
@@ -1,31 +1,32 @@
-import { useState } from "react";
+import { useState } from "react"
 
 type AddStudentFormProps = {
-	onAddStudent: ({ name }: { name: string }) => void;
-};
+  onAddStudent: ({ name }: { name: string }) => void
+}
 
 export default function AddStudentForm(props: AddStudentFormProps) {
-	const { onAddStudent } = props;
-	const [name, setName] = useState("");
+  const { onAddStudent } = props
+  const [name, setName] = useState("")
 
-	const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-		if (!name) return;
-		onAddStudent({ name });
-		setName("");
-	};
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    // if name is empty, return early this saves us from adding an empty student to the list of students in the Grid component (students state) and thus avoid unnecessary rerendering of the Grid component and its children. This is because React will not rerender if the state is the same as before. This is a performance optimization.
+    if (!name) return
+    onAddStudent({ name })
+    setName("")
+  }
 
-	return (
-		<form onSubmit={handleSubmit} className="add-student-form">
-			<label htmlFor="name">Navn</label>
-			<input
-				type="text"
-				id="name"
-				placeholder="Studentens navn"
-				value={name}
-				onChange={(e) => setName(e.target.value)}
-			/>
-			<button type="submit">Legg til student</button>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit} className='add-student-form'>
+      <label htmlFor='name'>Navn</label>
+      <input
+        type='text'
+        id='name'
+        placeholder='Studentens navn'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button type='submit'>Legg til student</button>
+    </form>
+  )
 }
--- live/dag-6/frontend/src/components/Grid.tsx
+++ live/dag-6/frontend/src/components/Grid.tsx
@@ -1,28 +1,30 @@
-import { useState } from "react";
-import Student from "./Student";
-import type { Student as StudentProp } from "./types";
-import AddStudentForm from "./AddStudentForm";
+import { useState } from "react"
+import Student from "./Student"
+import type { Student as StudentProp } from "./types"
+import AddStudentForm from "./AddStudentForm"
 
 type GridProps = {
-	students: StudentProp[];
-};
+  students: StudentProp[]
+}
 
 export default function Grid(props: GridProps) {
-	// props.students ?? []
-	const [students, setStudents] = useState<StudentProp[]>(props.students ?? []);
+  // props.students ?? []
+  const [students, setStudents] = useState<StudentProp[]>(props.students ?? [])
 
-	const onAddStudent = (student: { name: string }) => {
-		setStudents((prev) => [...prev, { id: crypto.randomUUID(), ...student }]);
-	};
+  const onAddStudent = (student: { name: string }) => {
+    // one liner under gjøres for å legge til en ny student i listen av studenter, dette for å unngå å mutere state direkte (students.push) og dermed unngå at React ikke oppdager endringen og dermed ikke rerender komponenten (Grid) som er avhengig av students state.
+    setStudents((prev) => [...prev, { id: crypto.randomUUID(), ...student }])
+  }
 
-	return (
-		<section>
-			<article className="grid">
-				{students.map((student) => (
-					<Student key={student.id} name={student.name} id={student.id} />
-				))}
-			</article>
-			<AddStudentForm onAddStudent={onAddStudent} />
-		</section>
-	);
+  return (
+    <section>
+      <article className='grid'>
+        {students.map((student) => (
+          // key brukes for å gjøre at react klarer å vite og skille mellom elementene som lages. Dette er for å kunne vite hvilke elementer som er endret, lagt til eller fjernet. Dette er for å kunne oppdatere DOMen på en effektiv måte.
+          <Student key={student.id} name={student.name} id={student.id} />
+        ))}
+      </article>
+      <AddStudentForm onAddStudent={onAddStudent} />
+    </section>
+  )
 }
